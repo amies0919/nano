3799ba196b52e5c0f66565828eb7865eafaad5bf
shennana <shennana@jd.com>
2016-07-04 11:49:39 +0800

    h5 component Base

--- src/js/H5ComponentBase.js
+++ src/js/H5ComponentBase.js
@@ -3,7 +3,7 @@ var H5ComponentBase = function (name, cfg) {
     var cfg = cfg || {};
     var id = ('h5_c_' + Math.random()).replace('.', '_');
     //把当前组件类型添加到样式中进行标记
-    var cls = 'h5_component_' + cfg.type + ' h5_component_name_' + name;
+    var cls = 'h5_component_name_' + name + ' h5_component_' + cfg.type;
     var component = $('<div class="h5_component ' + cls + '" id="' + id + '">');
     cfg.text && component.text(cfg.text);
     cfg.width && component.width(cfg.width / 2);
@@ -16,5 +16,16 @@ var H5ComponentBase = function (name, cfg) {
             left: '50%'
         });
     }
+    //很多自定义的参数
+    component.on('onLoad', function () {
+        component.addClass(cls + '_load').removeClass(cls + '_leave');
+        cfg.animateIn && component.animate(cfg.animateIn);
+        return false;
+    });
+    component.on('onLeave', function () {
+        component.addClass(cls + '_leave').removeClass(cls + '_load');
+        cfg.animateOut && component.animate(cfg.animateOut);
+        return false;
+    });
     return component;
 }
